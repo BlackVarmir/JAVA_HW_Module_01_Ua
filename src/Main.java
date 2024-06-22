656b28ac2fa0e049cc10bbf1c293ea4168e8cc4e
BlackVarmir <blackvarmir@gmail.com>
2024-06-22 08:08:33 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int size = 20;
 
         boolean trueandfalse = true;
